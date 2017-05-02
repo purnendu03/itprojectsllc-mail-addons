0dd1bdc2abe240a4b04225a2f1498b2b5b413ac1
purnendu03 <purnendusingh1985@gmail.com>
2017-05-02 17:03:48 +0530

    [IMP] update display string formate to hour:min, make it user specific so every user will get his own fetchmail timing

--- mail_check_immediately/models/models.py
+++ mail_check_immediately/models/models.py
@@ -28,12 +28,18 @@ class FetchMailServer(models.Model):
     def _fetch_mails(self):
         if self._context.get('run_fetchmail_manually'):
             # if interval less than 5 seconds
-            if self._last_updated and (datetime.datetime.now() - self._last_updated) < datetime.timedelta(0, 5):
-                raise exceptions.Warning(_('Error'), _('Task can be started no earlier than 5 seconds.'))
+            if self._last_updated and \
+                    (datetime.datetime.now() - self._last_updated) \
+                                                    < datetime.timedelta(0, 5):
+                raise exceptions.Warning(_('Error'),\
+                            _('Task can be started no earlier than 5 seconds.'))
 
         super(FetchMailServer, self)._fetch_mails()
 
-        res = self.env['fetchmail.server'].sudo().with_context(tz=self.env.user.tz).search([('state', '=', 'done')])
+        res = self.env['fetchmail.server'].sudo().\
+                        with_context(tz=self.env.user.tz).\
+                        search([('state', '=', 'done'),
+                                ('user', '=', self.env.user.login)])
         if res:
             res[0].run_time = self._run_time()
 
@@ -44,16 +50,31 @@ class FetchMailImmediately(models.AbstractModel):
 
     @api.model
     def get_last_update_time(self):
-        res = self.env['fetchmail.server'].sudo().with_context(tz=self.env.user.tz).search([('state', '=', 'done')])
+        res = self.env['fetchmail.server'].sudo()\
+                            .with_context(tz=self.env.user.tz)\
+                            .search([('state', '=', 'done'),
+                                     ('user', '=', self.env.user.login)])
         array = [r.run_time for r in res]
         if array:
-            return array[0]
+            last_update = datetime.datetime.strptime(array[0], \
+                                    tools.misc.DEFAULT_SERVER_DATETIME_FORMAT)
+            minutes = str(last_update.minute)
+            hours = str(last_update.hour)
+            if last_update.minute < 10:
+                minutes = '0' + str(last_update.minute)
+            if last_update.hour < 10:
+                hours = '0' + str(last_update.hours)
+                return
+            return hours + ':' + minutes
         else:
             return None
 
     @api.model
     def run_fetchmail_manually(self):
         fetchmail_task = self.env.ref('fetchmail.ir_cron_mail_gateway_action')
-        fetchmail_model = self.env['fetchmail.server'].sudo()
+        fetchmail_model = self.env['fetchmail.server'].sudo()\
+                            .with_context(tz=self.env.user.tz)\
+                            .search([('state', '=', 'done'),
+                                     ('user', '=', self.env.user.login)])
         fetchmail_task._try_lock()
         fetchmail_model.with_context(run_fetchmail_manually=True)._fetch_mails()
